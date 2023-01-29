f74456cffee2fb3e43cc802480963b364c3db7f7
root <root@DESKTOP-2ON1ADG>
2023-01-28 18:41:57 -0600

    feat/FTG-05 | Header markup
    Signed-off-by: root <root@DESKTOP-2ON1ADG>

---
+++ src/utils/dropdownBuilder.js
@@ -0,0 +1,33 @@
+export function optionsBuilder({ options }) {
+  const ul = document.createElement('ul');
+  const fragment = new DocumentFragment();
+
+  ul.setAttribute('role', 'listbox');
+
+  options.forEach(option => {
+    const item = listItem(option);
+    fragment.appendChild(item);
+  });
+
+  ul.appendChild(fragment);
+
+  return ul;
+}
+
+function listItem({ id, nodeLabel }) {
+  const li = document.createElement('li');
+  const label = document.createElement('label');
+  const input = document.createElement('input');
+  const labelText = document.createTextNode(nodeLabel);
+
+  label.setAttribute('for', id);
+  input.setAttribute('type', 'checkbox');
+  input.setAttribute('id', id);
+  input.setAttribute('name', id);
+
+  label.appendChild(input);
+  label.appendChild(labelText);
+  li.appendChild(label);
+
+  return li;
+}
